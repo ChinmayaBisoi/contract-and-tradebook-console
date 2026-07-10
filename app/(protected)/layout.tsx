ec2b6afa85199de372bade966c84d68c1ab7550e
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2026-07-10 12:52:36 +0530

    file name changes

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,29 +1,29 @@
-import SkeletonPageLoader from "@/components/loaders/skelton-page-loader";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import type React from "react";
 import { Suspense } from "react";
+import SkeletonPageLoader from "@/components/loaders/skeleton-page-loader";
 
 async function ProtectedLayout({ children }: { children: React.ReactNode }) {
-    const { isAuthenticated } = await auth();
+  const { isAuthenticated } = await auth();
 
-    if (!isAuthenticated) {
-        redirect("/");
-    }
+  if (!isAuthenticated) {
+    redirect("/");
+  }
 
-    return (
-        <div>
-            {children}
-        </div>
-    );
+  return (
+    <div>
+      {children}
+    </div>
+  );
 }
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <Suspense fallback={<SkeletonPageLoader />}>
-            <ProtectedLayout>
-                {children}
-            </ProtectedLayout>
-        </Suspense>
-    )
+  return (
+    <Suspense fallback={<SkeletonPageLoader />}>
+      <ProtectedLayout>
+        {children}
+      </ProtectedLayout>
+    </Suspense>
+  )
 }
