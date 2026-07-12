6cce71c9a269f655dd84c40ff0c1a31e873fd672
chinmayaBisoi3 <chinmaya@easyslr.com>
2026-07-12 23:28:30 +0530

    add todo

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -15,10 +15,12 @@ export default function DashboardPage() {
   return (
     <DashboardShell title="Dashboard">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        {/* 
+        // TODO - Maybe implement analytics using these components
         <SectionCards />
         <div className="px-4 lg:px-6">
           <ChartAreaInteractive />
-        </div>
+        </div> */}
         <DataTable data={data} />
       </div>
     </DashboardShell>
